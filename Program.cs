75d145d8eabbc15f79175e9523bfefc752021ef2
Noah Fogarty <nojosoft@gmail.com>
2022-11-10 12:26:18 -0500

    The Dominion of Kings
    Completes the challenge 'The Dominion of Kings'

--- Program.cs
+++ Program.cs
@@ -1,22 +1,39 @@
 ﻿#region software_information
 // Author: Noah Fogarty
 // Created: 10-Nov-2022
+// Version: 1.0
 
 string version = "1.0";
-Console.WriteLine("Chocolate Egg Divider - Version {0}", version);
+Console.WriteLine("Kingdom Score Calculator - {0}", version);
 
 Console.WriteLine("\n");
 #endregion
 
-#region console_output
-// Asks user how many chocolate eggs there are.
-Console.WriteLine("How many chocolate eggs are there?");
-int chocolateEggs = Convert.ToInt32(Console.ReadLine()); 
+#region variables
+int totalEstates; // Worth 1 point.
+int totalDuchies; // Worth 3 points.
+int totalProvinces; // Worth 6 points.
+int totalScore;
+#endregion
 
-// Calculates how many chocolate eggs to give to the four sisters, and how much is remaining to give to the duckbear.
-int eggsDivided = chocolateEggs / 4;
-int eggsRemainder = chocolateEggs % 4;
-Console.WriteLine("The total eggs each sister gets is {0}, the duckbear gets {1}", eggsDivided, eggsRemainder);
+#region console_output
+// Asks user to input how many estates, duchies and provinces they have.
+Console.WriteLine("How many estates do you have?");
+totalEstates = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("How many duchies do you have?");
+totalDuchies = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("How many provinces do you have?");
+totalProvinces = Convert.ToInt32(Console.ReadLine());
+
+// Calculates total score based on above answers.
+// Estates are worth 1 point, duchies are worth 3 points, and provinces are worth 6 points.
+totalScore = totalEstates + (totalDuchies * 3) + (totalProvinces * 6);
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Your total score is {0}", totalScore);
 
 Console.ReadLine(); // Pauses program.
 #endregion
